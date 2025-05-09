e5d4e877a207c8bc5662e5391f9947595920be80
takuteh <takumi.ktym@gmail.com>
2025-05-09 22:32:08 +0900

    正しくメモリマップの取得ができた

--- ZeianLoaderPkg/Main.c
+++ ZeianLoaderPkg/Main.c
@@ -13,9 +13,9 @@ struct MemoryMap {
 };
 
 EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
-  if (map->buffer == NULL) {
-    return EFI_BUFFER_TOO_SMALL;
-  }
+  // if (map->buffer == NULL) {
+  //   return EFI_BUFFER_TOO_SMALL;
+  // }
 
   map->map_size = map->buffer_size;
   return gBS->GetMemoryMap(
@@ -27,23 +27,29 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
 }
 
 EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
-  struct MemoryMap memmap;
+  // メモリマップを格納するためのバッファを確保（4096バイト×4）
+  CHAR8 memmap_buf[4096 * 4];
+  
+  // メモリマップを格納する構造体の初期化
+  // memmap_bufのサイズとポインタを指定してMemoryMap構造体を初期化
+  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+  
+  // GetMemoryMapを呼び出して、UEFIのメモリマップ情報をmemmap_bufに取得
+  GetMemoryMap(&memmap);
 
-  // 最初はNULLを渡して必要なサイズを取得
-  // 以下のパラメータを渡すとUEFIが必要なバッファサイズをmap_sizeに格納する
-  memmap.buffer_size = 0;
-  memmap.buffer = NULL;
-  EFI_STATUS status = GetMemoryMap(&memmap);
-  //最初はEFI_BUFFER_TOO_SMALLが返ってくるはず
-  if (status != EFI_BUFFER_TOO_SMALL) {
-    Print(L"Unexpected error: %r\n", status);
-    return status;
-  }
-
-  // map_size（必要なバッファサイズ）を表示
+  // 取得したメモリマップのサイズを表示
   Print(L"Memory map size: %lu bytes\n", memmap.map_size);
+  
+  // 1エントリ（1セグメント）のサイズを表示
+  Print(L"descriptor_size: %lu bytes\n", memmap.descriptor_size);
+  
+  // メモリマップ内のセグメント（エントリ）の個数を表示
+  // map_size（メモリマップ全体のサイズ）をdescriptor_size（1エントリのサイズ）で割った値がエントリ数
+  Print(L"entry count: %lu\n", memmap.map_size / memmap.descriptor_size);
 
-  while (1); // 停止
+  // プログラムを無限ループで停止（UEFIのアプリケーションは終了しない）
+  while (1);
 
   return EFI_SUCCESS;
 }
+
