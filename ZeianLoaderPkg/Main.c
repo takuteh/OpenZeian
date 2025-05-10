a01ca974b551d9c99d2d43cf3cd642ed269c3fbe
takuteh <takumi.ktym@gmail.com>
2025-05-10 22:10:19 +0900

    メモリマップをファイルに保存

--- ZeianLoaderPkg/Main.c
+++ ZeianLoaderPkg/Main.c
@@ -1,7 +1,11 @@
-#include <Uefi.h>
-#include <Library/UefiLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/MemoryAllocationLib.h>
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
 
 struct MemoryMap {
   UINTN buffer_size;
@@ -25,8 +29,103 @@ EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
       &map->descriptor_size,
       &map->descriptor_version);
 }
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+    case EfiReservedMemoryType: return L"EfiReservedMemoryType";
+    case EfiLoaderCode: return L"EfiLoaderCode";
+    case EfiLoaderData: return L"EfiLoaderData";
+    case EfiBootServicesCode: return L"EfiBootServicesCode";
+    case EfiBootServicesData: return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory: return L"EfiConventionalMemory";
+    case EfiUnusableMemory: return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode: return L"EfiPalCode";
+    case EfiPersistentMemory: return L"EfiPersistentMemory";
+    case EfiMaxMemoryType: return L"EfiMaxMemoryType";
+    default: return L"InvalidMemoryType";
+  }
+}
 
-EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
+// EFIのメモリマップ情報をfileに保存する関数
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+  CHAR8 buf[256];  // 出力用の一時バッファ（ASCII文字列）
+  UINTN len;       // 書き込みサイズ
+
+  // ヘッダ行の文字列（列名）を定義
+  CHAR8* header =
+    "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  len = AsciiStrLen(header);         // ヘッダの文字列長を取得
+  file->Write(file, &len, header);   // ファイルにヘッダを書き込む
+
+  // メモリマップの先頭アドレスとサイズを表示（デバッグ用）
+  Print(L"map->buffer = %08lx, map->map_size = %08lx\n",
+      map->buffer, map->map_size);
+
+  EFI_PHYSICAL_ADDRESS iter;  // メモリマップの現在位置を指すアドレス
+  int i;                      // インデックスカウンタ
+
+  // メモリマップを1エントリずつ処理
+  for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
+
+    // 現在の位置のメモリ記述子を取得
+    EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+
+    // 記述子の情報をフォーマットしてbufに書き込む
+    len = AsciiSPrint(
+        buf, sizeof(buf),
+        "%u, %x, %-ls, %08lx, %lx, %lx\n",         // CSV形式
+        i,                                         // インデックス
+        desc->Type,                                // メモリタイプ（数値）
+        GetMemoryTypeUnicode(desc->Type),          // メモリタイプ（名前）
+        desc->PhysicalStart,                       // 物理開始アドレス
+        desc->NumberOfPages,                       // ページ数
+        desc->Attribute & 0xffffflu);              // 属性（下位64bit）
+
+    // フォーマットした文字列をファイルに書き込む
+    file->Write(file, &len, buf);
+  }
+
+  // 正常終了を返す
+  return EFI_SUCCESS;
+}
+
+
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+  EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+
+  // 実行中のこのUEFIアプリケーションが格納されているデバイス情報を取得
+  gBS->OpenProtocol(
+      image_handle,
+      &gEfiLoadedImageProtocolGuid,
+      (VOID**)&loaded_image,
+      image_handle,
+      NULL,
+      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  // そのデバイスが使っているファイルシステムの操作インターフェースを取得
+  gBS->OpenProtocol(
+      loaded_image->DeviceHandle,
+      &gEfiSimpleFileSystemProtocolGuid,
+      (VOID**)&fs,
+      image_handle,
+      NULL,
+      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  // ファイルシステムからルートディレクトリ（"/"）を開いて使えるようにする
+  fs->OpenVolume(fs, root);
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
   // メモリマップを格納するためのバッファを確保（4096バイト×4）
   CHAR8 memmap_buf[4096 * 4];
   
@@ -46,6 +145,21 @@ EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *Syste
   // メモリマップ内のセグメント（エントリ）の個数を表示
   // map_size（メモリマップ全体のサイズ）をdescriptor_size（1エントリのサイズ）で割った値がエントリ数
   Print(L"entry count: %lu\n", memmap.map_size / memmap.descriptor_size);
+  
+  // ルートディレクトリの操作インターフェースの取得
+  EFI_FILE_PROTOCOL* root_dir;
+  OpenRootDir(image_handle, &root_dir);
+
+  // memmapというファイルを開いて操作インターフェースを取得
+  EFI_FILE_PROTOCOL* memmap_file;
+  root_dir->Open(
+      root_dir, &memmap_file, L"\\memmap",
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+
+  SaveMemoryMap(&memmap, memmap_file);
+
+  //操作インターフェースに用意されているClose関数でファイルを閉じる
+  memmap_file->Close(memmap_file);
 
   // プログラムを無限ループで停止（UEFIのアプリケーションは終了しない）
   while (1);
